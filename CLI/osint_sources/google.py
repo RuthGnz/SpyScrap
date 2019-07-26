e3042456e32e985b38023dfa8dc2a771580c3e7d
Miguel000 <miguelhernandez2907@gmail.com>
2019-07-26 14:57:00 +0200

    Add info in google

--- CLI/osint_sources/google.py
+++ CLI/osint_sources/google.py
@@ -14,13 +14,14 @@ def google(toSearch,placeToSearch,knownImage):
 	print(toSearch)
 	chrome_path = './chromedriver_linux64/chromedriver'
 	driver = webdriver.Chrome(chrome_path)
-	if placeToSearch != '':
+	if placeToSearch != None:
 		driver.get("https://www.google.com/search?q=site:"+placeToSearch+"+AND+%22"+toSearch+"%22&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiz2eSN_9vgAhUJoRQKHU8YCuwQ_AUIDigB&biw=1181&bih=902")
 	else:
 		driver.get("https://www.google.com/search?q="+toSearch+"&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiz2eSN_9vgAhUJoRQKHU8YCuwQ_AUIDigB&biw=1181&bih=902")
 
 	driver.implicitly_wait(50)
 
+	'''
 	isMoreButton=True
 	while isMoreButton:
 		for i in range(1,10):
@@ -43,37 +44,53 @@ def google(toSearch,placeToSearch,knownImage):
 				input_elem.click()
 			except:
 				break
-
-
+	'''
+	out = []
 	jsonfile={}
+	t =""
 	search=driver.find_elements_by_tag_name('img')
-	j=0
+	j=1
 	print('Retrieving images')
 	now = datetime.datetime.now()
 	os.mkdir( "images/"+str(now) );
 	for s in search:
 		td_p_input = s.find_element_by_xpath('..')
+		#text = td_p_input.find_element_by_xpath('..')
 		link=td_p_input.get_attribute('href')
+		try:
+			listtext = driver.find_element_by_xpath("//*[@id=\"rg_s\"]/div["+str(j)+"]/a[2]")
+			t = listtext.get_attribute("text")
+		except:
+			pass
+
+		print(t)
 		if link != None:
 			url=link.split('imgurl=')
 			if len(url)>1:
 				imgUrl=url[1].split('&')[0]
 				imgUrl = unquote(imgUrl)
-				name=os.path.join('images/'+str(now),str(j)+"-"+placeToSearch+"-"+toSearch+".jpg")
+				if placeToSearch != None:
+					name=os.path.join('images/'+str(now),str(j)+"-"+placeToSearch+"-"+toSearch+".jpg")
+				else:
+					name=os.path.join('images/'+str(now),str(j)+"-"+toSearch+".jpg")
 				jsonfile[name]=imgUrl
-				
-				try:
-					urllib.request.urlretrieve(imgUrl, name)
-				except:
-					src=s.get_attribute('src')
-					if src != None:
-						urllib.request.urlretrieve(src, name)
+				print (imgUrl)
+				jsonfile["info"] = t
+				t=""
+				if knownImage != None:
+					try:
+						urllib.request.urlretrieve(imgUrl, name)
+					except:
+						src=s.get_attribute('src')
+						if src != None:
+							urllib.request.urlretrieve(src, name)
 				j=j+1
-
+				out.append(jsonfile)
+				jsonfile={}
 	path=os.path.join('images/'+str(now),'google_data.json')
 	if not knownImage:
 		with open(path, 'w+') as outfile:
-			json.dump(jsonfile, outfile)
+			json.dump(out, outfile)
 	else:
 		identify(knownImage,path)
 
@@ -86,7 +103,7 @@ def identify(unknown_image_path,path):
 
 	newJsonData={}
 
-	for f in onlyfiles:	
+	for f in onlyfiles:
 		known_image = face_recognition.load_image_file(unknown_image_path)
 		try:
 			unknown_image = face_recognition.load_image_file(join('./images', f))
