1e491e6c5cc8ae8fd5e5931d4ed4496da8879879
ruth.gonzalez.novillo <ruth.gonzalez.novillo.next@bbva.com>
2019-08-06 13:41:35 +0200

    CLI module separated

--- back/src/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './uploads'
-app.config['ALLOWED_EXTENSIONS'] = ['png','jpeg','jpg']
-CORS(app)
-
